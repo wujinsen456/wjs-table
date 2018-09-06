533f9e312a6e81338ab7618a84f303239eb5bb04
wujinsen456 <1098206859@qq.com>
2018-09-06 22:49:30 +0800

    second

--- js/index.js
+++ js/index.js
@@ -2,13 +2,13 @@
  * Created by 三木 on 2018/9/5.
  */
     let td=document.querySelectorAll("td");
-    //        alert(td.length);
+
 
     td.forEach((items)=>{
-        items.style.width="83px";
+        // items.style.width="83px";
         items.ondblclick=function(){
             let input=document.createElement("input");
-            input.style="outline:none;border:none;width:80px";
+            // input.style="outline:none;border:none;width:80px";
             this.innerText="";
             input.value=this.innerText;
             items.appendChild(input);
@@ -20,10 +20,56 @@
     });
 
     let button=document.querySelectorAll("button");
-    button.forEach((v,i)=>{
+    button.forEach((v)=>{
         v.onclick=function(){
             let tr=v.parentNode.parentNode;
             console.log(tr.parentNode);             //tbody
             tr.parentNode.removeChild(tr);
         }
-    })
+    });
+
+
+    function excel(){
+
+
+        let items = document.querySelectorAll("td");
+
+        items.forEach(function (v) {
+            v.ondblclick = function () {
+                let input = document.createElement("input");
+                input.value = v.innerHTML;
+                v.innerHTML = "";
+                v.appendChild(input);
+                input.focus();
+                input.onblur = function () {
+                    v.innerHTML = input.value;
+                }
+            }
+        });
+
+        let add=document.querySelector("#add");
+        let tbody=document.querySelector("tbody");
+        add.onclick=function () {
+            let tr=document.createElement("tr");
+            tr.innerHTML="<td>1</td>"+"<td>2</td>"+"<td>3</td>"+
+                "<td>4</td>"+"<td>5</td>"+"<td>6</td>"+"<td>7</td>"+"<td>8</td>"
+                +"<td>9</td>"+"<td>10</td>"+"<td><button>删除</button></td>";
+            tbody.appendChild(tr);
+
+            // let button1=document.querySelectorAll("button");
+            // console.log(button1);
+            // button1.forEach((e)=>{
+            //     e.onclick=function(){
+            //         let tr=e.parentNode.parentNode;
+            //         console.log(tr.parentNode);             //tbody
+            //         tr.parentNode.removeChild(tr);
+            //     }
+            // });
+        };
+
+
+    }
+
+
+
+    excel();
